92a31e0bb50de10aa27c8da2189e5f9d9f1bd8ea
pakaplace <pleeplace@gmail.com>
2019-07-18 01:34:41 +0200

    added postgres url

--- easy_lover_backend/middlewares/Sequelize.js
+++ easy_lover_backend/middlewares/Sequelize.js
@@ -9,7 +9,7 @@ const loggingOpts = {
   // other levels can define custom functions as needed
 };
 
-if (process.env.NODE_ENV === "test") {
+if (process.env.NODE_ENV === "local") {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     operatorsAliases: false,
@@ -24,27 +24,17 @@ if (process.env.NODE_ENV === "test") {
     }
   });
 } else {
-  console.log("NODE_ENV not defined");
-  console.log(
-    process.env.POSTGRES_DB,
-    process.env.POSTGRES_HOST,
-    process.env.POSTGRES_PORT
-  );
-  sequelize = new Sequelize({
-    // username: process.env.POSTGRES_USER,
-    // password: process.env.POSTGRES_PW,
-    database: process.env.POSTGRES_DB,
-    host: process.env.POSTGRES_HOST,
+  sequelize = new Sequelize(process.env.POSTGRES_URL, {
     dialect: "postgres",
-    port: process.env.POSTGRES_PORT || "5432"
-    // operatorsAliases: false,
-    // logging: loggingOpts[process.env.SEQUELIZE_LOGGING || 0]
-    // dialectOptions: {
-    //   statement_timeout: 10000
-    // },
-    // define: {
-    //   paranoid: true
-    // }
+    operatorsAliases: false,
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: true,
+      statement_timeout: 5000
+    },
+    define: {
+      paranoid: true
+    }
   });
 }
 
