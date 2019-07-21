b808eac60ceb750ee5ea8c5c40623d56124b09f8
pakaplace <pleeplace@gmail.com>
2019-07-21 19:58:14 +0200

    fixed twilio url link

--- app.js
+++ app.js
@@ -30,9 +30,7 @@ app.get("/sendlink/:phoneNumber", async (req, res, next) => {
   }
   try {
     let message = await client.messages.create({
-      body: `Your link is https://${process.env.HOST_URL}/${
-        foundUser.phoneNumber
-      }`,
+      body: `Your link is https://${process.env.HOST_URL}/user/${foundUser.id}`,
       from: process.env.TWILIO_PROD_NUMBER,
       to: foundUser.phoneNumber
     });
